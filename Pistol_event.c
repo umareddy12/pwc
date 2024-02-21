1e6a4d2d19e8a8c495f2149ac365f374182f4073
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2024-02-21 23:15:55 +0530

    Pistol_event.c

---
+++ Pistol_event.c
@@ -0,0 +1,25 @@
+//Assignment: Pistol Event
+BEGIN
+
+READ variable num, sum,count
+
+SET sum=0,count=0
+
+READ num
+
+WHILE sum<=100
+
+sum=sum+num
+
+count=count+1
+
+READ num
+
+END WHILE
+
+ PRINT "The number of turns is "+ count
+
+END
+
+
+
