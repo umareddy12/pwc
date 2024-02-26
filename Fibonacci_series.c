f35b503133bd6e5772dbd61ad545b2f3398da3cf
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2024-02-26 19:30:14 +0530

    Fibonacci_series.c

---
+++ Fibonacci_series.c
@@ -0,0 +1,6 @@
+The first two terms in the Fibonacci series are 0 and 1, respectively and each subsequent term is the sum of the previous two.
+
+Using this definition to calculate the first several terms in the sequence, we get:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ... Find the series up-to 100. Write a pseudocode for the same.
+
