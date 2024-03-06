2d5353586d65fb4e727236d4f13c4ebcf7afb134
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2024-03-06 22:49:44 +0530

    Maximum_num.c

---
+++ Maximum_num.c
@@ -0,0 +1,14 @@
+//PSEUDOCODE FOR FINDING MAXIMUM NUMBER
+BEGIN 
+DECLARE variable index,maximum,array[10]
+FOR index<-0 to 9 DO
+READ array[index]
+END FOR 
+SET maximum<-array[0]
+FOR index <-0 to 9 DO
+IF maximum <array[index]
+maximum<-array[index]
+END IF 
+END FOR
+PRINT maximum
+END
