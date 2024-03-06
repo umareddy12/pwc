e541f397436a4ad47ac2961afdbabfbe7baf1b3f
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2024-03-06 22:46:34 +0530

    Array_1D.c

---
+++ Array_1D.c
@@ -0,0 +1,13 @@
+//PSEUDOCODE FOR 1-D ARRAY
+BEGIN
+DECLARE variables index,sum,average,marks[5]
+SET index=0,sum=0
+FOR  index <- 0 yo 4
+FOR  index<-0 to 4 do
+READ marks[index]
+sum<-sum+marks[index]
+i<-i+1
+END FOR
+average <-sum/5
+PRINT average
+END
